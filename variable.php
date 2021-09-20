cd4bee1941c9ee8ee0ac5988c13ed567693e2a6c
IliusSagar <sagar01830@gmail.com>
2021-09-20 10:26:20 -0700

    Variables in PHP

---
+++ variable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Variable</title>
+</head>
+<body>
+
+
+	<?php
+
+		$name="Mohammad Sagar";
+		echo $name;
+
+
+
+	?>
+
+
+
+</body>
+</html>
