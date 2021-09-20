8e976ad31541bff692f783ac5c2e5c718768f8c4
IliusSagar <sagar01830@gmail.com>
2021-09-20 10:48:52 -0700

    Datatypes in php

---
+++ datatypes.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Data types</title>
+</head>
+<body>
+
+
+	<h2>Data Types</h2>
+
+
+	<?php 
+
+	// // $x="String";
+	// $x=12345.5;
+
+
+	// if($x){
+	// 	echo "true";
+	// }else{
+	// 	echo "false";
+	// }
+
+	// echo $x;
+
+	// echo "<br/>";
+
+
+
+	// var_dump($x);
+
+
+	//$laptops = array("HP","LENOVO","MAC");
+	//print_r($laptops);
+
+	//echo $laptops[1];
+
+
+	$x=null;
+
+	var_dump($x);
+
+
+	?>
+
+</body>
+</html>
